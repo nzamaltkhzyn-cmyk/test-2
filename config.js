457a264921243774d3d7115c9512e15fc1b7bd18
nzamaltkhzyn-cmyk <nzamaltkhzyn@gmail.com>
2025-08-26 08:12:23 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// هذا الملف يحتوي على المفاتيح الحساسة ويجب عدم رفعه إلى GitHub
+// This file contains sensitive keys and should NOT be committed to GitHub.
+
+const SUPABASE_URL = "https://inexkigotfyhsprhbqyn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZXhraWdvdGZ5aHNwcmhicXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNTc4NDcsImV4cCI6MjA3MTYzMzg0N30.nkDBDL-BFHaTJAUdxzXrPqOzpWonDyr2jkKO2S_UwbU";
